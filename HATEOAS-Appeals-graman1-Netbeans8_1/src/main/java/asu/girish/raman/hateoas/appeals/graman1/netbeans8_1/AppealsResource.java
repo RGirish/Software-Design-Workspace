f5f4ece052a7b9217092a5fb6c7320018b6313bd
Girish Raman <rgirish1994@gmail.com>
2016-04-13 10:57:56 -0700

    Removed other projects from Netbeans, Started with HATEOAS

---
+++ HATEOAS-Appeals-graman1-Netbeans8_1/src/main/java/asu/girish/raman/hateoas/appeals/graman1/netbeans8_1/AppealsResource.java
@@ -0,0 +1,12 @@
+package asu.girish.raman.hateoas.appeals.graman1.netbeans8_1;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Path;
+
+@Path("appeals")
+public class AppealsResource {
+
+    @Context
+    private UriInfo context;
+}
